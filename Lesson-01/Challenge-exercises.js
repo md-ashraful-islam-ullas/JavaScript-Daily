5484679d39d9259a97e6123b14759add10a2a9c2
md-ashraful-islam-ullas <ashraful.ullas@gmail.com>
2026-07-11 09:29:50 +0600

    initial commit with lesson 1 done

---
+++ Lesson-01/Challenge-exercises.js
@@ -0,0 +1,11 @@
+// 1h. You order a T-shirt for $10, socks for $8, and dinner plates for $20. Use JavaScript to calculate the total cost of your order.
+
+10 + 8 + 20;
+
+// 1i. Your bank account has $100, you spend $20 on lunch, $50 on dinner, and earn $200 from your job. Calculate how much money you have.
+
+100 - 20 - 50 + 200;
+
+// 1j. Use document.body.innerHTML = ...; to make the web page blank.
+
+document.body.innerHTML = "";
---
+++ Lesson-01/Practice-problem.js
@@ -0,0 +1,28 @@
+// 1a. Use alert(...); to display 'Good morning!' in a popup.
+
+alert("Good Morning!");
+
+// 1b. Display your name in a popup.
+
+alert("Ashraful");
+
+// 1c. Using math, calculate 10 + 5 in the Console.
+
+10 + 5;
+
+// 1d. Calculate 20 - 5 in the Console.
+
+20 - 5;
+
+// 1e. Calculate 2 + 2 - 5 in the Console.
+
+2 + 2 - 5;
+
+
+// 1f. Use document.body.innerHTML = ...; to display 'Good morning!' on the web page.
+
+document.body.innerHTML = "Good Morning";
+
+// document.body.innerHTML = "Good Morning";
+
+document.body.innerHTML = "Ashraful";
